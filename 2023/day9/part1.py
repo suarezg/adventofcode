2cc6b5e760e5fa7ff7c2f1d992231479bb7a4168
Gin Suarez <ginnsuarez@gmail.com>
2023-12-09 16:36:57 -0700

    Day 9 solution

---
+++ 2023/day9/part1.py
@@ -0,0 +1,17 @@
+
+def extrapolate(numbers) -> int:
+    if all(n == 0 for n in numbers):
+        return 0
+    
+    differences = [t - s for s, t in zip(numbers, numbers[1:])]
+    addition = extrapolate(differences)
+    return differences[-1] + addition
+
+with open('input.txt') as file:
+    result = 0
+    for line in file:
+        numbers = [int(num) for num in line.strip().split()]
+        result += extrapolate(numbers) + numbers[-1]
+
+
+    print(f"Result: {result}")
